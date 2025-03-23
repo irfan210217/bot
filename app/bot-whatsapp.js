54032ce9c18c7d142fcf8256640e51de470036ed
irfan210217 <irfannsuprianto@gmail.com>
2025-03-23 17:34:19 +0700

    new version 2.0

--- app/bot-whatsapp.js
+++ app/bot-whatsapp.js
@@ -5,19 +5,23 @@ import qrcode from "qrcode";
 import { messageResponse } from "../src/components/depositBalance.js"
 
 export const start = async (req, res) => {
-    const whatsapp = new Client();
-
-    whatsapp.on('qr', (qr) => {
-        let code = qrcode.toFile("./app/images/qrcode.png", qr)
-        fs.createReadStream('./app/images/qrcode.png').pipe(res)
-    })
-
-    whatsapp.on('ready', () => {
-        res.redirect('/');
-        res.send("success");
-    })
-
-    whatsapp.on('message_create', messageResponse);
-
-    whatsapp.initialize();
+    try {
+        const whatsapp = new Client();
+    
+        whatsapp.on('qr', (qr) => {
+            qrcode.toFile("./app/images/qrcode.png", qr)
+            fs.createReadStream('./app/images/qrcode.png').pipe(res)
+        })
+    
+        whatsapp.on('ready', () => {
+            res.redirect('/');
+            res.send("success");
+        })
+    
+        whatsapp.on('message_create', messageResponse);
+    
+        whatsapp.initialize();
+    } catch (error) {
+        console.log(error);
+    }
 };
