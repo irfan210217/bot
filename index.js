215cd1946b511bd6b558ee4971468c3d928c2072
irfan210217 <irfannsuprianto@gmail.com>
2025-03-23 19:39:02 +0700

    new update

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 import express from "express";
-import axios from "axios";
-import { start } from "./app/bot-whatsapp.js"
+// import { start } from "./app/bot-whatsapp.js"
 
 const app = express();
 
 app.get('/api/wa', (req, res) => {
-    start((data) => {
-        res.send(data);
-    })
-})
-
-app.get('/', async (req, res) => {
-    await axios.get('bot-whatapp-tawny.vercel.app/api/wa');
     res.send({
-        msg : "success"
+        msg : true
     })
 })
 
