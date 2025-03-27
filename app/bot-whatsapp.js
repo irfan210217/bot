9b7ec3caf5ac4cb07ec5efb8ff6f1aa9a1f33753
irfan210217 <irfannsuprianto@gmail.com>
2025-03-27 19:45:28 +0700

    new2

--- app/bot-whatsapp.js
+++ app/bot-whatsapp.js
@@ -4,15 +4,15 @@ import { messageResponse } from "../src/components/depositBalance.js"
 
 export const start = (callback) => {
     try {
-        const whatsapp = new Client();
+        // const whatsapp = new Client();
 
-        // const whatsapp = new Client({
-        //     puppeteer: {
-        //         product: "chrome",
-        //         executablePath: "/usr/bin/chromium-browser",
-        //         args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless']
-        //     }
-        // });
+        const whatsapp = new Client({
+            puppeteer: {
+                product: "chrome",
+                executablePath: "/usr/bin/chromium-browser",
+                args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless']
+            }
+        });
 
         whatsapp.on('qr', (qr) => {
             let signID = `nbm${Math.random().toString(36).substring(2, 10)}Vipergo`;
